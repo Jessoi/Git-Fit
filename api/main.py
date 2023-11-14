0cff77bce5bb9410083afc1a954a461d7c207d3f
Jesse Z <yzhang1005@gmail.com>
2023-11-14 17:41:28 -0600

    vite converted version

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
+        os.environ.get("CORS_HOST", "http://localhost:5173")
     ],
     allow_credentials=True,
     allow_methods=["*"],
