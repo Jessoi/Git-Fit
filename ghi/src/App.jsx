91b5ac7e374aaa3686a68b7becee583969b9846b
Daniel Glasgow <danielrossglasgow@gmail.com>
2023-11-20 15:08:11 -0600

    Nav Pages Made

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './HomePage'
 import Nav from './Nav'
+import TraineeForm from './Profiles/TraineeForm'
+import LoginForm from './Profiles/LoginForm'
+import ProfileView from './Profiles/TraineeProfile'
+import CreateWorkout from './Workouts/CreateWorkout'
 
 function App() {
   return (
@@ -11,6 +15,13 @@ function App() {
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
+
+            <Route path="trainee">
+              <Route path="" element={<ProfileView/>} />
+              <Route path="signup" element={<TraineeForm/>} />
+              <Route path="login" element={<LoginForm/>} />
+              <Route path="createworkout" element={<CreateWorkout/>} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ ghi/src/Profiles/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function LoginForm() {
+    return (
+        <div>
+            <h1>Log In</h1>
+        </div>
+    );
+}
+
+export default LoginForm;
