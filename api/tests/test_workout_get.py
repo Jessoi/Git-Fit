e35e861f6ee976ea4fde4ef7623606c15c22e991
Preston Ford <preston.ford93@gmail.com>
2023-12-06 17:10:48 -0500

    partial unit test added.

--- api/tests/test_workout_get.py
+++ api/tests/test_workout_get.py
@@ -1,46 +1,38 @@
-from pydantic import BaseModel
-from queries.workouts import WorkoutOut
+from fastapi.testclient import TestClient
+from main import app
+from queries.workouts import WorkoutRepository, WorkoutOut
 
 
-class WorkoutIn(BaseModel):
-    userid: int
-    name: str
-    intensity: str
-    favorite: bool
+client = TestClient(app)
 
 
-# class WorkoutOut(BaseModel):
-#     workoutid: int
-#     userid: int
-#     name: str
-#     intensity: str
-#     favorite: bool
-
-
-class ListWorkoutOut(BaseModel):
-    workouts: list[WorkoutOut]
-
-
-class FavoriteIn(BaseModel):
-    favorite: bool
-
-
-def fake_get_userworkouts():
-    return WorkoutOut(
-        workoutid=1,
-        userid=1,
-        name="legs and glutes",
-        intensity="High",
-        favorite=False,
-    )
+class WorkoutTestQueries:
+    def mock_get_userworkouts():
+        return WorkoutOut(
+            workoutid=1,
+            userid=1,
+            name="legs and glutes",
+            intensity="High",
+            favorite=False,
+            workout_datetime=None,
+        )
 
 
 def test_get_userworkouts():
-    # Arrange
-    pass
+    app.dependency_overrides[WorkoutRepository] = WorkoutTestQueries
 
-    # act
+    response = client.get("/1/workouts")
+    assert response.status_code == 200
+    assert response.json() == {
+        "workoutid": 1,
+        "userid": 1,
+        "name": "legs and glutes",
+        "intensity": "High",
+        "favorite": False,
+        "workout_datetime": None,
+    }
 
-    # cleanup
+    response = client.get("/8/workouts")
+    assert response.status_code == 404
 
-    # assert
+    app.dependency_overrides[WorkoutRepository] = None
