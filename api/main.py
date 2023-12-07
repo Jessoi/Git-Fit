24323a12be8fea6d7418f9174cd78d2d531aaf55
Jesse Z <yzhang1005@gmail.com>
2023-12-07 12:06:47 -0600

    reverting cors changes

--- api/main.py
+++ api/main.py
@@ -12,9 +12,7 @@ app.include_router(workouts.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:5173"),
-    #eslint-disable-next-line react-hooks/exhaustive-deps
-    "https://module3-project-gamma-baby-boys-dad1dc47618ee01452cc68b60125316.gitlab.io"],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
