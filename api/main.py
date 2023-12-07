0552842c0f05beadf5785d8ebe587153395eb952
Jesse Z <yzhang1005@gmail.com>
2023-12-07 14:00:10 -0600

    yml change

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ from routers import exercises
 app = FastAPI()
 app.include_router(workouts.router)
 
-url = "https://baby-boys.gitlab.io" # noqa
+url = "https://baby-boys.gitlab.io"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[os.environ.get("CORS_HOST", url), "http://localhost:5173"],
