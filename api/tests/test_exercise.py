5fe353c5ab30d2a20f696b7c1db5ab1b04491513
Daniel Glasgow <danielrossglasgow@gmail.com>
2023-12-10 12:59:19 -0600

    commit to pull

--- api/tests/test_exercise.py
+++ api/tests/test_exercise.py
@@ -1,71 +1,41 @@
 import pytest
 from fastapi.testclient import TestClient
+from pydantic import BaseModel
 from main import app
-from queries.exercises import ExerciseOut, ExerciseQueries
+from queries.exercises import ExerciseQueries
 
 client = TestClient(app)
 
-class
+class MockExerciseOut(BaseModel):
+    exerciseid: int
+    name: str
+    muscle: str
+    difficulty: str
+    instructions: str
 
-class MockExercise:
-    def test_add_exercise(self, exerciseid: int):
-        if exerciseid != 1:
-            raise Exception("Invalid exercieid")
+class MockExercise():
+    def get_all_exercises(self):
+        return [MockExerciseOut(
+                exerciseid= 1,
+                name= "curls",
+                muscle= "bicep",
+                difficulty= "easy",
+                instructions= "test"
+            )]
 
-    def test_get_exercise(self, exerciseid: int) -> list:
-        if exerciseid != 1:
-            return []
-        return ({"placeid": "testPlaceId"})
 
-@pytest.fixture(autouse=True)
-def setup_tests():
-    app.dependency_overrides[ExerciseQueries] = lambda: MockExercise()
-    app.dependency_overrides = lambda: fake_exercise_data
-    yield
-
-def test_get_fake_exercise():
+def test_get_exercise():
+    app.dependency_overrides[ExerciseQueries] = MockExercise
     response = client.get("/api/exercises")
+
     assert response.status_code == 200
-    assert response.json() == [{"placeid": "testPlaceId"}]
+    response_data = [{
+                "exerciseid": 1,
+                "name": "curls",
+                "muscle": "bicep",
+                "difficulty": "easy",
+                "instructions": "test"
+            }]
+    assert response.json() == response_data
 
-def test_get_fake_exercise_not_found():
-    response = client.get("/api/exercises")
-    assert response.status_code == 500
-
-
-# from fastapi.testclient import TestClient
-# from unittest.mock import Mock, patch
-# from main import app
-# from queries.exercises import ExerciseOut, ExerciseQueries
-
-# client = TestClient(app)
-
-# class MockExerciseQueries:
-#     def get_all_exercises(self):
-#         return [
-#             ExerciseOut(
-#                 exerciseid=1,
-#                 name="curls",
-#                 muscle="bicep",
-#                 difficulty="easy",
-#                 instructions="lorim"
-#             )
-#         ]
-
-# @patch("exercises.ExerciseQueries", MockExerciseQueries)
-# def test_get_fake_exercise():
-#     response = client.get("/api/exercises/")
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         "exerciseid": 1,
-#         "name": "curls",
-#         "muscle": "bicep",
-#         "difficulty": "easy",
-#         "instructions": "lorim"
-#     }
-
-# @patch("exercises.ExerciseQueries", MockExerciseQueries)
-# def test_get_fake_exercise_not_found():
-#     response = client.get("/api/exercises/")
-#     assert response.status_code == 404
-#     assert response.json() == {"detail": "Item not found"}
+    app.dependency_overrides = {}
